efa0f311136d476c0f03165c716cfe75d27e8b7c
extremeheat <extreme@protonmail.ch>
2021-02-07 02:43:50 -0500

    fix cli, add example

--- cli.js
+++ cli.js
@@ -4,9 +4,10 @@ const compile = require('./compiler')
 if (!process.argv[2]) {
     console.warn('usage: protoyaml <inputYAML> [outputJSON]')
     console.warn('example: protoyaml proto.yaml protocol.json')
-
+} else {
     const inp = process.argv[2]
-    const out = process.argv[3] || inp
+    const out = process.argv[3] || (inp.split('.', -1)[0] + '.json')
 
     compile(inp, out)
+    console.info('✔ ', out)
 }
