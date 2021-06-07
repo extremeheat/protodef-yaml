fc05eaf7c1ba641147d076d1a99014905cf8e7b7
extremeheat <extreme@protonmail.ch>
2021-06-07 17:39:43 -0400

    web/editor: add note on indentation

--- docs/editor/app.js
+++ docs/editor/app.js
@@ -1,7 +1,7 @@
 var editor = ace.edit("editor")
 editor.setTheme("ace/theme/xcode")
 editor.session.setMode("ace/mode/yaml")
-editor.session.setValue("!StartDocs: true # All comments below are apart of the doc\n\n# # Hello world!\n# This is some markdown, with a horizontal rule (___) and block terminator (===) \n# at the end which tells the parser to flush and write all of the comments.\n# ___\n# ===\n\n# Reads a vector 3f\nvec3:\n    x: f32\n    y: f32\n    z: f32\n\n## Uncomment below for an example packet:\n# packet_some_packet:\n#     !id: 4\n#     # Map a i32 to a string\n#     status: i32 =>\n#         0: active\n#         1: inactive\n#     ## Anonymous switch statement\n#     _: status ?\n#         if active:\n#             # read a position\n#             position: vec3[]varint\n#     ## Inline switch\n#     offset: status ?\n#         if active: vec3[]varint")
+editor.session.setValue("!StartDocs: true # All comments below are apart of the doc\n\n# # Hello world!\n# This is some markdown, with a horizontal rule (___) and block terminator (===) \n# at the end which tells the parser to flush and write all of the comments.\n# Make sure to be consistent with tabulation, don't mix spaces and tabs.\n# ___\n# ===\n\n# Reads a vector 3f\nvec3:\n    x: f32\n    y: f32\n    z: f32\n\n## Uncomment below (select, Control + /) for an example packet:\n# packet_some_packet:\n#     !id: 4\n#     # Map a i32 to a string\n#     status: i32 =>\n#         0: active\n#         1: inactive\n#     ## Anonymous switch statement\n#     _: status ?\n#         if active:\n#             # read a position\n#             position: vec3[]varint\n#     ## Inline switch\n#     offset: status ?\n#         if active: vec3[]varint")
 let timer
 
 function clearErrorState() {
